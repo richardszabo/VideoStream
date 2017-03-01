59eed6549509b83d9e4e824c65844d45c833673e
Rics <richard_szabo@jataka.hu>
2017-03-01 14:51:16 +0100

    Adding a MainActivity above MainSenderActivity with a Send and a Receive button. Moving MainSenderActivity's permission handling to a separate PermissionHandler class. This class is used by MainActivity instead of MainSenderActivity.

---
+++ VideoStreamAndroid/app/src/main/java/hu/rics/videostreamandroid/MainActivity.java
@@ -0,0 +1,54 @@
+package hu.rics.videostreamandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    PermissionHandler permissionHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        permissionHandler = new PermissionHandler(this);
+        permissionHandler.requestPermission();
+
+        View.OnClickListener senderListener = new View.OnClickListener() {
+
+            public void onClick(View v) {
+                if( permissionHandler.hasRights() ) {
+                    Intent intent = new Intent(MainActivity.this, MainSenderActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast toast = Toast.makeText(MainActivity.this, "No right to start", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        };
+
+        final Button senderButton = (Button) findViewById(R.id.senderButton);
+        senderButton.setOnClickListener(senderListener);
+
+        View.OnClickListener receiverListener = new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, "Receiver", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        };
+
+        final Button receiverButton = (Button) findViewById(R.id.receiverButton);
+        receiverButton.setOnClickListener(receiverListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        permissionHandler.onRequestPermissionsResult( requestCode, permissions, grantResults);
+    }
+}
--- VideoStreamAndroid/app/src/main/java/hu/rics/videostreamandroid/MainSenderActivity.java
+++ VideoStreamAndroid/app/src/main/java/hu/rics/videostreamandroid/MainSenderActivity.java
@@ -30,19 +30,15 @@ public class MainSenderActivity extends AppCompatActivity implements View.OnClic
     MediaRecorderWrapper mediaRecorderWrapper;
     Button startButton;
     TextView ipTextView;
-    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 123;
-    boolean hasRights = false;
     StreamingCameraPreview streamingCameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sender);
-        if( requestPermission() ) {
-            hasRights = true;
-            streamingCameraPreview = new StreamingCameraPreview(this);
-            mediaRecorderWrapper = new MediaRecorderWrapper(this,R.id.camera_preview,streamingCameraPreview);
-        }
+
+        streamingCameraPreview = new StreamingCameraPreview(this);
+        mediaRecorderWrapper = new MediaRecorderWrapper(this,R.id.camera_preview,streamingCameraPreview);
         ipTextView = (TextView) findViewById(R.id.ipTextView);
         try {
             ipTextView.setText("IP address:" + getIPAddress(true));
@@ -57,7 +53,7 @@ public class MainSenderActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onResume() {
         super.onResume();
-        if( hasRights && mediaRecorderWrapper != null ) {
+        if( mediaRecorderWrapper != null ) {
             mediaRecorderWrapper.startPreview();
         }
     }
@@ -65,7 +61,7 @@ public class MainSenderActivity extends AppCompatActivity implements View.OnClic
     @Override
     protected void onPause() {
         super.onPause();
-        if( hasRights && mediaRecorderWrapper != null ) {
+        if( mediaRecorderWrapper != null ) {
             mediaRecorderWrapper.stopPreview();
         }
     }
@@ -83,106 +79,6 @@ public class MainSenderActivity extends AppCompatActivity implements View.OnClic
             mediaRecorderWrapper.startPreview();
             startButton.setText("Stop");
         }*/
-    }
-
-    boolean requestPermission() {
-        // permission check (https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en)
-        List<String> permissionsNeeded = new ArrayList<>();
-
-        final List<String> permissionsList = new ArrayList<>();
-        if (!addPermission(permissionsList, Manifest.permission.CAMERA)) {
-            Log.i(TAG,"permissionsNeeded.add(\"Camera\");");
-            permissionsNeeded.add("Camera");
-        }
-        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Log.i(TAG,"permissionsNeeded.add(\"Storage\");");
-            permissionsNeeded.add("External storage");
-        }
-
-        if (!addPermission(permissionsList, Manifest.permission.RECORD_AUDIO)) {
-            Log.i(TAG,"permissionsNeeded.add(\"Audio\");");
-            permissionsNeeded.add("Audio");
-        }
-        if (!addPermission(permissionsList, Manifest.permission.INTERNET)) {
-            Log.i(TAG,"permissionsNeeded.add(\"Internet\");");
-            permissionsNeeded.add("Internet");
-        }
-        Log.i(TAG,"onCreate:" + permissionsList.size() + ":" + permissionsNeeded.size());
-        if (permissionsList.size() > 0) {
-            if (permissionsNeeded.size() > 0) {
-                // Need Rationale
-                String message = "You need to grant access to " + permissionsNeeded.get(0);
-                for (int i = 1; i < permissionsNeeded.size(); i++)
-                    message = message + ", " + permissionsNeeded.get(i);
-                showMessageOKCancel(message,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(MainSenderActivity.this,permissionsList.toArray(new String[permissionsList.size()]),
-                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
-                            }
-                        });
-                return false;
-            }
-            ActivityCompat.requestPermissions(this,permissionsList.toArray(new String[permissionsList.size()]),
-                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
-            return false;
-        }
-        return true;
-    }
-
-    private boolean addPermission(List<String> permissionsList, String permission) {
-        if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
-            permissionsList.add(permission);
-            // Check for Rationale Option
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,permission)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
-            {
-                Map<String, Integer> perms = new HashMap<>();
-                // Initial
-                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.RECORD_AUDIO, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.INTERNET, PackageManager.PERMISSION_GRANTED);
-                // Fill with results
-                for (int i = 0; i < permissions.length; i++) {
-                    Log.i(TAG,"i:" + i + ":" + permissions[i] + ":" + grantResults[i] );
-                    perms.put(permissions[i], grantResults[i]);
-                }
-                // Check for ACCESS_FINE_LOCATION
-                if( perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-                        && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                        && perms.get(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
-                        && perms.get(Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-                    // All Permissions Granted
-                    hasRights = true;
-                } else {
-                    // Permission Denied
-                    Toast.makeText(this, "Some Permission is Denied", Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-            break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
     }
 
     /**
